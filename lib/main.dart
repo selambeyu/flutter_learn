6f98da075044d0d3cff3a8a6a836b93a166ceb89
melkam beyene <melkam3392beyu@gmail.com>
2019-12-20 00:03:49 -0800

    add product file

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,61 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  runApp(MyApp());
 
-// #docregion MyApp
-class MyApp extends StatelessWidget {
-  // #docregion build
+}
+
+class MyApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}
+
+ 
+
+class _MyAppState extends State<MyApp>{
+  List<String> _products =['Food Tester'];
   @override
   Widget build(BuildContext context) {
+    var a="hello";
+    // TODO: implement build
     return MaterialApp(
-      title: 'Startup Name Generator',
-      home: RandomWords(),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("EasyList"
+          ) ,
+          ),
+        body: Column(children: [
+         Container(
+           margin: EdgeInsets.all(10.0),
+           child: RaisedButton(
+             onPressed: (){
+                setState(() {
+                  _products.add("Advanced value tester");
+                 print(_products);
+                });
+             },
+             child: Text("Add product"),
+           ),
+         ),
+          Column(children:  _products.map((element) => Card(
+            child: Column(children: <Widget>[
+              Image.asset('assets/images/food.jpeg', width: double.maxFinite, fit: BoxFit.fitWidth),
+              Text(element)
+            ],
+            ),
+
+          )).toList(),),
+
+        ],
+
+        )),
+        );
+
+
   }
-// #enddocregion build
-}
-// #enddocregion MyApp
 
-// #docregion RWS-var
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  // #enddocregion RWS-var
-
-  // #docregion _buildSuggestions
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-  // #enddocregion _buildSuggestions
-
-  // #docregion _buildRow
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
-  // #enddocregion _buildRow
-
-  // #docregion RWS-build
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-// #enddocregion RWS-build
-// #docregion RWS-var
-}
-// #enddocregion RWS-var
-
-class RandomWords extends StatefulWidget {
-  @override
-  RandomWordsState createState() => RandomWordsState();
 }
