d76585a56c20bf8aefc483ebed4f6b29d018333b
melkam beyene <melkam3392beyu@gmail.com>
2019-12-20 00:40:14 -0800

    add productmanager file

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,37 @@
 import 'package:flutter/material.dart';
 
+
+import './product_manager.dart';
+
 void main(){
   runApp(MyApp());
 
 }
 
-class MyApp extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return _MyAppState();
-  }
 
-}
-
- 
-
-class _MyAppState extends State<MyApp>{
-  List<String> _products =['Food Tester'];
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    var a="hello";
-    // TODO: implement build
-    return MaterialApp(
+  var a="hello";
+  // TODO: implement build
+  return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
           title: Text("EasyList"
-          ) ,
-          ),
-        body: Column(children: [
-         Container(
-           margin: EdgeInsets.all(10.0),
-           child: RaisedButton(
-             onPressed: (){
-                setState(() {
-                  _products.add("Advanced value tester");
-                 print(_products);
-                });
-             },
-             child: Text("Add product"),
-           ),
-         ),
-          Column(children:  _products.map((element) => Card(
-            child: Column(children: <Widget>[
-              Image.asset('assets/images/food.jpeg', width: double.maxFinite, fit: BoxFit.fitWidth),
-              Text(element)
-            ],
-            ),
+      ) ,
+      ),
+      body: ProductManager(),
 
-          )).toList(),),
-
-        ],
-
-        )),
-        );
+  ),
+  );
 
 
   }
 
+
 }
+
+
+
+
+
