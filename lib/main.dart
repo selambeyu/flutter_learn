2bfaa6eafbe554f9ca922f3c3aaa04d082e2776f
melkam beyene <melkam3392beyu@gmail.com>
2019-12-21 05:28:07 -0800

    add productmain file

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,16 @@
 import 'package:flutter/material.dart';
 
 
-import './product_manager.dart';
-
 void main(){
   runApp(MyApp());
-
 }
 
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  // TODO: implement build
-  return MaterialApp(
-      home: Scaffold(
-      appBar: AppBar(
-          title: Text("EasyList"
-      ) ,
-      ),
-      body: ProductManager("Food Tester"),
-
-  ),
-  );
-
-
+    // TODO: implement build
+    return null;
   }
 
-
 }
-
-
-
-
-
