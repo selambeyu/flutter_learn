98cb2bd84ec97ff0f8eb82876104355896e195ac
melkam beyene <melkam3392beyu@gmail.com>
2019-12-21 12:01:22 -0800

    add randomwordain file

---
+++ lib/randomWordMain.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import './_randomWords.dart';
+
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    // TODO: implement build
+    return MaterialApp(
+
+      home: RandomWord(),
+    );
+  }
+
+}
+
+
+
