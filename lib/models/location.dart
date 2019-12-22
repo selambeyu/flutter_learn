19541f9c6c33724debc084a2b9e97cea4eb45be2
melkam beyene <melkam3392beyu@gmail.com>
2019-12-22 09:23:11 -0800

    add location fact model

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/material.dart';
+
+import './location_fact.dart';
 
 class Location {
   final String name;
   final String imagePath;
   final List<LocationFact>fact;
 
-  Location(this.name, this.imagePath, this.fact)
+  Location(this.name, this.imagePath, this.fact);
 
 }
 
-class LocationFact {
-
-};
