bb532900c00b5185f2b008fd3d1eba1b1b14b198
melkam beyene <melkam3392beyu@gmail.com>
2019-12-22 09:19:40 -0800

    add location model

---
+++ lib/models/location.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Location {
+  final String name;
+  final String imagePath;
+  final List<LocationFact>fact;
+
+  Location(this.name, this.imagePath, this.fact)
+
+}
+
+class LocationFact {
+
+};
