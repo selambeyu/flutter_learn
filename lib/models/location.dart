4292f6c194e44218c4f0e324940e6038a9a66210
melkam beyene <melkam3392beyu@gmail.com>
2019-12-22 10:04:59 -0800

    mapping the data

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -8,5 +8,16 @@ class Location {
 
   Location(this.name, this.imagePath, this.fact);
 
+  static List<Location> fetchAll(){
+    return [
+      Location("Ertale","assets/images/ertale.jpeg",
+          [LocationFact("Dallol","Dallol (Amharic: ዳሎል) is a locality in the Dallol woreda of northern Ethiopia")]),
+      Location("Abay Nile","assets/images/abay.jpeg",
+          [LocationFact("Tis abay","The Blue Nile Falls is a waterfall on the Blue Nile river in Ethiopia.")]),
+      Location("Chilad Baboon","assets/images/chilad.jpeg",
+          [LocationFact("Semen monunt","In the Semien Mountains that many call the Roof of Africa,")])
+    ];
+  }
+
 }
 
--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -2,31 +2,39 @@ import 'package:flutter/material.dart';
 
 import './text_section.dart';
 import './image_banner.dart';
+import '../../models/location.dart';
 
 
 
 class LocationDetail extends StatelessWidget{
+
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location= locations.first;
     // TODO: implement build
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple[900]),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Location Detail"),
+          title: Text(location.name),
         ),
         body: Column(
          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ImageBanner("./assets/images/chilada.jpeg"),
-            TextSection("Chilada Baboon","The gelada, sometimes called the bleeding-heart monkey or the gelada \"baboon\", is a species of Old World monkey found only in the Ethiopian Highlands."),
-            TextSection("Section one","this section contain the overview about turism"),
-            TextSection("Section two","this section contain the detail about tourism in ethiopia"),
-          ],
+            ImageBanner(location.imagePath),
+
+          ]..addAll(textSection(location)),
         ),
       ),
     );
+  }
+
+  List<Widget> textSection(Location location){
+    return location.fact.map((fact)=> TextSection(
+      fact.title,fact.text
+    )).toList();
   }
 
 }
